8f3eae696935bc7952e6143b3381de965150c087
Znina-bilel <bilelzn13@gmail.com>
2024-11-20 13:37:54 +0100

    ici on a changer les setters par un constructeur

--- essai.php
+++ essai.php
@@ -1,25 +1,28 @@
 <?php
-
+ 
 declare(strict_types=1);
-
+ 
 class Pont
 {
-   // Définition de la propriété statique. Elle sera partagée
-   public static int $nombrePietons = 0;
-  
-   // Je laisse volontairement la méthode non statique.
-   // Seule la référence à la propriété est importante.
-   public function nouveauPieton()
-   {
-       // Mise à jour de la propriété statique.
-       self::$nombrePietons++;
-   }
+    private float $longueur;
+    private float $largeur;
+ 
+//*  public function setLongueur(float $longueur): void
+ //  {
+ //    $this->longueur = $longueur;
+ // }
+ 
+ //public function setLargeur(float $largeur): void
+ //{
+//  $this->largeur = $largeur;
+ //}
+public function __construct(float $longueur, float $largeur)
+{
+    $this->longueur = $longueur;
+    $this->largeur = $largeur;
 }
-
-$pontLondres = new Pont;
-$pontLondres->nouveauPieton();
-
-$pontManhattan = new Pont;
-$pontManhattan->nouveauPieton();
-
-echo Pont::$nombrePietons;
+}
+ 
+$towerBridge = new Pont(286.0, 62.0);
+//$towerBridge->setLongueur(286.0);
+//$towerBridge->setLargeur(62.2);
