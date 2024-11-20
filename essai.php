32a333d11ec4523d1429f1bc8ccde665dd19a48c
Znina-bilel <bilelzn13@gmail.com>
2024-11-20 10:19:48 +0100

    on a essayer de modifier la date mais cette fois il ne sera changé car on a utuliser l'instance DateTimeImmutable

--- essai.php
+++ essai.php
@@ -1,10 +1,8 @@
 <?php
  
-$date = new DateTime;
+$date = new DateTimeImmutable;
 $newDate = $date->modify('+1 day');
 
-echo $date->format('d/m/y').PHP_EOL;
-echo $newDate->format('d/m/y').PHP_EOL;
+echo $date->format('d/m/Y').PHP_EOL;
 
-$formatedDate = $date->modify('+1 dat')->format('d/m/y');
-echo $formatedDate.PHP_EOL;
+echo $date->format('d/m/Y').PHP_EOL;
