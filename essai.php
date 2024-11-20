d3fa79901d648c2ec87086e211e885f417daf24e
Znina-bilel <bilelzn13@gmail.com>
2024-11-20 11:59:43 +0100

    ici on a essayer de calculer la surface en affichant une resultat de type string en utilisant private string unite = m2

--- essai.php
+++ essai.php
@@ -1,26 +1,22 @@
 <?php
 
+declare(strict_types=1);
 
-declare(strict_types =1);
 class Pont
 {
+  // $unite ne sert que dans la classe, on met cette propriété en privé.
+   private string $unite = 'm²';
+  
+   public float $longueur;
+   public float $largeur;
+  
+   public function getSurface(): string
+   {
+       return ($this->longueur * $this->largeur) . $this->unite; // on renvoie l’unité en plus de la surface
+   }}
 
-public float $longueur;
-public float $largeur;
-
-public function getSurface(){
-
-return $surface = $this->longueur * $this->largeur;
-var_dump($surface);
-}
-}
 $towerBridge = new Pont;
 $towerBridge->longueur = 286.0;
 $towerBridge->largeur = 15.0;
-$towerBridgeSurfcae = $towerBridge->getSurface();
-var_dump($towerBridgeSurfcae);
-$manhattanBridge = new Pont;
-$manhattanBridge->longueur = 25.0;
-$manhattanBridge->largeur = 15.0;
-$manhattanBridgeSurface = $manhattanBridge->getSurface();
-var_dump($manhattanBridgeSurface);
+
+echo $towerBridge->getSurface();
