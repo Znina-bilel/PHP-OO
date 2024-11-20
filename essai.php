aff5d5f58dbead191c8c204bf0b44dcffd55663d
Znina-bilel <bilelzn13@gmail.com>
2024-11-20 10:09:40 +0100

    ici on a essayer d'affcher le resultat sans et avec chainage

--- essai.php
+++ essai.php
@@ -1,4 +1,10 @@
 <?php
  
 $date = new DateTime;
-echo $date->format('d/m/y');
+$newDate = $date->modify('+1 day');
+
+echo $date->format('d/m/y').PHP_EOL;
+echo $newDate->format('d/m/y').PHP_EOL;
+
+$formatedDate = $date->modify('+1 dat')->format('d/m/y');
+echo $formatedDate.PHP_EOL;
