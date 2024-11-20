5d07c6293e8c383d8d60c5565167aefc9051968e
Znina-bilel <bilelzn13@gmail.com>
2024-11-20 11:27:31 +0100

    cette fois on a calculer deux nouvelle surface en appelent la fonction getSurface()

--- essai.php
+++ essai.php
@@ -1,14 +1,26 @@
 <?php
 
-declare(strict_types=1);
 
+declare(strict_types =1);
 class Pont
 {
-   public float $longueur;
-   public float $largeur;
-  
-   public function getSurface(): float
-   {
-       return $this->longueur * $this->largeur;
-   }
+
+public float $longueur;
+public float $largeur;
+
+public function getSurface(){
+
+return $surface = $this->longueur * $this->largeur;
+var_dump($surface);
 }
+}
+$towerBridge = new Pont;
+$towerBridge->longueur = 286.0;
+$towerBridge->largeur = 15.0;
+$towerBridgeSurfcae = $towerBridge->getSurface();
+var_dump($towerBridgeSurfcae);
+$manhattanBridge = new Pont;
+$manhattanBridge->longueur = 25.0;
+$manhattanBridge->largeur = 15.0;
+$manhattanBridgeSurface = $manhattanBridge->getSurface();
+var_dump($manhattanBridgeSurface);
