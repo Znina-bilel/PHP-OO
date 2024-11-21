3380f2b2001d8fff61cf0d8fa36d72d2413dcd68
Znina-bilel <bilelzn13@gmail.com>
2024-11-21 11:14:20 +0100

    J'ai essayer maintenantt de faire un exercice , voila sa premeire partie d'ou j'ai creer une class Personne , j'ai fait les getters et les setters et puis j'ai fait l'affichage de cette personne

--- essai.php
+++ essai.php
@@ -1,56 +1,39 @@
 <?php
- 
-declare(strict_types=1);
- 
-class User
+class Personne {
+    private $id;
+    private $nom;
+
+    function __construct(int $id, string $nom)
+    {
+        $this->nom = $nom;
+        $this->id =$id;
+    }
+    function ajoutPersonne(){
+
+    }
+public function getNom()
 {
-    public const STATUS_ACTIVE = 'active';
-    public const STATUS_INACTIVE = 'inactive';
- 
-    public function __construct(public string $username, public string $status = self::STATUS_ACTIVE)
-    {
-    }
-
-    public function setStatus(string $status): void
-    {
-        if (!in_array($status, [self::STATUS_ACTIVE, self::STATUS_INACTIVE])) {
-            trigger_error(sprintf('Le status %s n\'est pas valide. Les status possibles sont : %s', $status, implode(', ', [self::STATUS_ACTIVE, self::STATUS_INACTIVE])), E_USER_ERROR);
-        };
-
-        $this->status = $status;
-    }
-
-    public function getStatus(): string
-    {
-        return $this->status;
-    }
+   return $this->nom;
 }
- 
-class Admin extends User
+public function getId()
 {
-    // Ajout d'un tableau de roles pour affiner les droits des administrateurs :)
-    public function __construct(public string $username, public array $roles = [], public string $status = self::STATUS_ACTIVE)
-    {
-    }
- 
-    // Méthode d'ajout d'un rôle, puis on supprime les doublons avec array_filter.
-    public function addRole(string $role): void
-    {
-        $this->roles[] = $role;
-        $this->roles = array_filter($this->roles);
-    }
- 
-    // Méthode de renvoie des rôles, dans lequel on définit le rôle ADMIN par défaut.
-    public function getRoles(): array
-    {
-        $roles = $this->roles;
-        $roles[] = 'ADMIN';
- 
-        return $roles;
-    }
-
-    public function setRoles(array $roles): void
-    {
-        $this->roles = $roles;
-    }
+    return $this->id;
 }
+public function setId($id)
+{
+     $this->id = $id;
+}
+public function setNom($nom)
+{
+     $this->nom = $nom;
+}
+}
+
+$p=new Personne(1,"bilel");
+echo($p->getId()."  ".$p->getNom());
+
+
+
+
+
+
